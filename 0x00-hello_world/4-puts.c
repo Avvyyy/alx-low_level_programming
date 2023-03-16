3ae9aa34bfd01d649c0bc5a9b82ee185f5018917
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 15:05:20 -0700

    Using the puts() function todisplay a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-/* Program to print a statement using the puts function */
 #include <stdio.h>
 
 /** 
@@ -9,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");`
 	return (0);
 }
