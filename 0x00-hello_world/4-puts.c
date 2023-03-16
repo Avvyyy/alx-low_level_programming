6d6aa5d1227a243985e17fdd92d63975d47ee91e
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 15:14:16 -0700

    Using the puts() function todisplay a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - this is the funcion housing all other functions in c
  * void: shows us that there is no argument to be passed to the function
  * Return: 0 if the code is succesful
