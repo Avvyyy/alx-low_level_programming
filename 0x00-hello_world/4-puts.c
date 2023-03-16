413e2549e529e050fbe4313420572ee56ae02089
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 14:04:34 -0700

    Program to print a sentence using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 /* Program to print a statement using the puts function */
 #include <stdio.h>
 
-/**uses the puts()function to print a statement&*/
-int main()
+/*uses the puts()function to print a statement*/
+int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,4 +1,4 @@
 1. Script to print the preprocessing stage of one file into an output file
 2. Script to print the complied version of a file
 3. Script to print the assembly code of a .c file and generates the .c file
-Program to print a sentence in c using the puts()
+4. Program to print a sentence in c using the puts()
