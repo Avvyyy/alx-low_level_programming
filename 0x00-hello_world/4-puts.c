2efc070561c20906a7180f1c8bbcb00761aac93f
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 14:10:24 -0700

    Program to print a sentence using puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 /* Program to print a statement using the puts function */
 #include <stdio.h>
 
-/*uses the puts()function to print a statement*/
+/** 
+ * main -this is the funcion housing all other functions in c
+ * void: shows us that there is no argument to be passed to the function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
