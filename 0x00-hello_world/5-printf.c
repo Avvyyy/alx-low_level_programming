411b180a8e13400603b1e29dacf04d8b34061b72
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 14:43:27 -0700

    Intorducing the printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,9 @@
  * void: tells us that the main function ccepts no argumnets
  * Return: 0 if code is succesful
 */
-int main (void)
+`
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
