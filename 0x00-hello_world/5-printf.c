7be0c7bdb98978c9565b1e12986df12062bccbf1
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-16 14:27:40 -0700

    Program to print a sentence using theh printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+/*Program to print a sentnce using the printf() function*/
 #include <stdio.h>
 
 /**
@@ -5,7 +6,7 @@
  * void: tells us that the main function ccepts no argumnets
  * Return: 0 if code is succesful
 */
-int main(void)
+int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
