344df1a6ef90cb705f40f76a57f6e57fb1304d75
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-17 13:13:18 -0700

    Program to print the first 10 counting numbers in C

---
+++ 5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function housing other functions 
+ * void: Shows that function talkes no arguments
+ * return: 0 if program is successful
+*/
+
+int main(void)
+{
+	int i=0;
+
+	for(;i<=10;i++)
+		printf("%d", i);
+	return(0);
+}
