e3e40dc944a1cb4cfa88b5014006e6bcdc96f4bd
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-21 11:57:50 -0700

    First Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+/*Program to print _putchar to the standard output using main.h*/
+
+#include "main.h"
+
+/**
+ * main - function that contains the program flow
+ *
+ * Return: 0 (if program is sucessful)
+ */
+
+int main(void)
+{
+	_input();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+/*Program to print _putchar*/
+
+#include <stdio.h>
+#include <stdilb.h>
+
+/**
+ * _input - Function to print a constant to standard output
+ *
+ * Return: void (returns no argument)
+ */
+
+void _input(void)
+{
+	char command[8] = "_putchar";
+	putchar(command);
+	return;
+}
