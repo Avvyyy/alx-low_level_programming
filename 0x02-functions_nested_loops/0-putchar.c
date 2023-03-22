7fc85e955f07a31093e00d714da6d2e091173982
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-22 01:28:20 -0700

    Fourth Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-/*Program to print _putchar to the standard output using main.h*/
-
 #include "main.h"
 
 /**
- * main - function that contains the program flow
+ * main - Prints _putchar as a message followed by a new line.
  *
- * Return: 0 (if program is sucessful)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_putchar \n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count = 0, c_size;
+
+	c_size = sizeof(str) / sizeof(int);
+	for (count = 0; count < c_size; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
+
