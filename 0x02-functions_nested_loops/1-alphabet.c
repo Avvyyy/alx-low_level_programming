ba0336855132375b422e70fa5c9d89da63a754fc
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-22 01:57:54 -0700

    Last Commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - prints the alphabet 10 times
+ * print_alphabet - prints the alphabet in lowercase
  *
  * Return: None.
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	int i;
-	int j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 97; i < 123; i++)
 	{
-		for (i = 97; i < 123; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 }
