65b6bd827ba20f2dd3e3274a98d23f8fc1399d1b
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-23 12:00:50 -0700

    Final edit on all files in yythis direcotry

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,33 @@
-#nclude "main.h"
+#include "main.h"
 
 /**
- * print_triangle - Program to print a triangle in the terminal
- * @size : Size of the triangle to be printed
- *
- * Return: None
+ * print_triangle - entry point
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
  */
 
 void print_triangle(int size)
 {
+	int row, hashes, spaces;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (int i = 1; i <= size; i++)
+	else
 	{
-		for (int j = 1; j <= i; j++)
+		for (row = 1; row <= size; row++)
 		{
-			_putchar('#')
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
