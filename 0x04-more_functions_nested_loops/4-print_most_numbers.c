b7f9fda7f51112aa0c2983d7b931f7724f30e489
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-23 11:40:14 -0700

    Test on file 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 *print_most_numbers - prints 0 to 9 but leaves out 2 and 4
-*@c: ascii of digit
+*@c: Digit to be printed
 *Return: none
 */
 
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 
 	while (c <= '9')
 	{
-		if (c != '2' && c != '4')
+		if (!(c == '2' && c == '4'))
 		{
 			_putchar(c);
 			c++;
