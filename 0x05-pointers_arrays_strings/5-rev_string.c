3f111364c92c40d318ca00feaa0dd84edc9760bb
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-03-27 12:34:10 -0700

    Programs to reset the value of a pinter to 98, swap 2 integers, reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - Function to print the a string reversed
+ * @s: Pointer housing string to be reversed
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int 1 = 0;
+	while (s[i] != '\0')
+		i++;
+	while (i > 0)
+	{
+		i--;
+		_putchar(s);
+	}
+}
