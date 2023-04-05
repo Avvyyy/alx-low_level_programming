9507dcbebc0f2ae43fca8005d03a621d0f4419af
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-05 08:19:41 -0700

    Program to print a string and a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function to print a string
+ * @s: Pointer to the string to be printed
+ *
+ * Return: NONE
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
