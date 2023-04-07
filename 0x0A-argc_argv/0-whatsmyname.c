595dae0a1529e9432db51950a3984f7e8979a758
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-07 12:45:13 -0700

    First Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Function to print the name of a C source file
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: None
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
