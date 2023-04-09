5eb3bc3bbd62e771b5593b1b27b92d75b0fdff5e
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-09 13:45:32 -0700

    Update on file 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+/**
+ * isNum - check if char in string array is num
+ * @num: string
+ * Return: 0 if it's num, 1 if not
+ */
+
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0 ; i < l ; i++)
+		if (!isdigit(num[i]))
+			return (1);
+	return (0);
+}
 
 /**
  * main - Function to print the sum  of positive numbers
@@ -12,30 +29,24 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 1, sum = 0;
+	int i, sum;
 
-	if (argc < 2)
+	if (argc == 1)
 		printf("0\n");
 	else
 	{
-		while (i < argc)
+		sum = 0;
+		for (i = 1 ; i < argc ; i++)
 		{
-			if (isdigit(*argv[i]))
-			{
+			if (isNum(argv[i]) == 0)
 				sum += atoi(argv[i]);
-				i++;
-			}
 			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-
-
 		}
 		printf("%d\n", sum);
 	}
-
-
 	return (0);
 }
