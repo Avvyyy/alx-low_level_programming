515b016bc791a73fac4b2c00a9dad74a9a3280b0
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-09 12:27:14 -0700

    Creation of script to create a static library with all c files in current directory

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#listing all the c files in the directory
+$c_file = $(ls *.c);
+
+#Converting the .c to .o
+for file in $c_file
+do
+	gcc -c $file
+done
+
+#Converting to .a
+
+ar rc liball.a *.o;
+
+
+#Removing the .o for good practice
+
+rm *.o;
