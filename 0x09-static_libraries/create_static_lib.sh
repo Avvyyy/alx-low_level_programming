f6cb373a85b172c9ac8a8edef64f31a4d88df965
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-09 12:57:33 -0700

    Update on the script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Converting the .c to .o
-gcc -c *.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
 
 # Converting to .a
-ar rc liball.a *.o
+ar -rc liball.a *.o
 
 #Indexing the files in the library
 ranlib liball.a
-
-# Removing the .o files for good practice
-rm *.o
 
