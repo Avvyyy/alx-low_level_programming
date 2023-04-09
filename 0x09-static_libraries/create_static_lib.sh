5b33fdff9ba736a9e9a225fc63970f117fe0fa1e
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-09 12:54:38 -0700

    Update on the script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Listing all the c files in the directory
-c_file=$(ls *.c)
-
 # Converting the .c to .o
-for file in $c_file
-do
-        gcc -c $file
-done
+gcc -c *.c
 
 # Converting to .a
 ar rc liball.a *.o
