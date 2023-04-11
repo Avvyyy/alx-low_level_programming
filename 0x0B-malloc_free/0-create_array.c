4780ca08791b7e93ab469d7d344635e37128c23f
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-11 13:18:21 -0700

    First Commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,10 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *myAr = (char *)malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
-
-	char *myAr = (char *)malloc(size * sizeof(char));
 
 	if (myAr  == NULL)
 		return (NULL);
