1a983391ac48c02e61b07e8d0459d7a56f1f29c5
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-17 15:14:03 -0700

    Update on files 2 and 3

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * init_dog - Function that initilaises a variable of type struct dog
+ * @d: New pointer variable of type struct dog
+ * name: Pointer variable to name
+ * age: Age of the dog
+ * owner: Pointer to name of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
