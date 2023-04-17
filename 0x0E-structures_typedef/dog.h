a25dc8d5f0d0b82b5bacc67cea0d2493ebefe81c
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-17 14:29:31 -0700

    First Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Structure for a new dog
+ * @name: Pointer to name of dog
+ * @age: Name of dog
+ * owner: Pointer to name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
