f8c1263a99c97d8c69d961ccff897824846d7949
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-27 16:27:17 -0700

    Update on lits.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,10 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d} %s\n", h->len, h->str);
-
-		j++;
+		
 		h = h->next;
+		j++;
+
 	}
 
 	return (j);
