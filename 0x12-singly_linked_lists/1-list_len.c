3359f514f4166f87b64a0b828825fd8e7db140b5
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-27 15:03:48 -0700

    Function to print the number of nodes in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: Pointer to list_t
+ *
+ * Return: The number of nodes in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodesNum = 0;
+
+	while (h != NULL)
+	{
+		nodesNum ++;
+		h = h->next;
+	}
+
+	return (nodesNum);
+}
