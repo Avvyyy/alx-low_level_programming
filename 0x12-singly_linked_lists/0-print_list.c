15bda9cf6d1b3d2dd5123f32a76bff7a26ee23ae
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-27 14:41:24 -0700

    Function to print the content of list_t

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list: Functionto print all elements in a list list_t
+ * @h: Pointer to list to be printed
+ *
+ * Return: Numbe of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int j;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d} %s\n", h->len, h->str);
+
+		j++;
+		h = h->next;
+	}
+
+	return (j);
+}
+
+
