b4da9c4777509a4bf1fdbe90224939f13baa0e98
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-04-28 00:51:25 -0700

    Fixing betty issues on 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		
+
 		h = h->next;
 		j++;
 
@@ -25,5 +25,3 @@ size_t print_list(const list_t *h)
 
 	return (j);
 }
-
-
