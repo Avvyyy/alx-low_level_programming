6041ad4748447c8c04ba01ac5f0f27a091252c1d
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-05-02 15:02:17 -0700

    Correction of compilation error on file 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL;
+	if (new_node == NULL)
 		return (NULL);
 
 	 new_node->n = n;
@@ -34,10 +34,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	while (temp != NULL && i < idx - 1)
+	while (temp != NULL && count < idx - 1)
 	{
 		temp = temp->next;
-		i++;
+		count++;
 	}
 
 	if (temp == NULL)
