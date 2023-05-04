5e1874293a8ce0362d8ae85a276b3e60d41d07ad
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-05-04 12:50:55 -0700

    Correction of betty issues on files 5 and 6

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - finds the number of bits to flip to get from one number to another
+ * flip_bits - finds the number of bits to flip to
+ * get from one number to another
  * @n: the first number
  * @m: the second number
  *
@@ -18,5 +19,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		x >>= 1;
 	}
 
-	return count;
+	return (count);
 }
