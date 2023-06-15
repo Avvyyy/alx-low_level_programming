a4493ea8c6f1f5c4d974e11e44857485f18cb57a
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-15 14:19:09 -0700

    Creation of 8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Function to delete a node at a given index
+ * @head: Pointer to the head node
+ * @index: Index of the node to be deleted
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	if (*head == NULL)
+		return (-1);
+
+	dlistint_t *current = *head;
+	unsigned int i = 0;
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+
+	return (1);
+}
