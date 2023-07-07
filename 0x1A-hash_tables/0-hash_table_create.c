dd66c841cdae1d7c05a09bed528f4d66b1c351c1
Avvvyy <uzochukwu.favour310@gmail.com>
2023-07-07 06:38:42 -0700

    CReation of hash node array in 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (!new_hash_table)
 		return (NULL);
 
 	new_hash_table->size = size;
+	new_hash_table->array = malloc(size * sizeof(hash_node_t *));
+	
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table->array);
+		return (NULL);
+	}
+
+	for(i = 0; i < size; i++)
+		new_hash_table->array[i] = NULL;
 
 	return (new_hash_table);
 }
