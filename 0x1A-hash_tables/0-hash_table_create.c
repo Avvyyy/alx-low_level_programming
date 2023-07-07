9e815719c574d82025746b74d0c8d1ace3f6996c
Avvvyy <uzochukwu.favour310@gmail.com>
2023-07-07 10:37:35 -0700

    Last Commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,14 +18,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_hash_table->size = size;
 	new_hash_table->array = malloc(size * sizeof(hash_node_t *));
-	
 	if (!new_hash_table->array)
 	{
 		free(new_hash_table->array);
 		return (NULL);
 	}
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		new_hash_table->array[i] = NULL;
 
 	return (new_hash_table);
